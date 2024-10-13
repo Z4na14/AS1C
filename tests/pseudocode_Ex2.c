ea260694b0055cff731db83f763d067c0dff1346
Jorge Saghin <zanajorgesaghin@gmail.com>
2024-10-13 13:50:43 +0200

    Almost finished ex 2

--- tests/pseudocode_Ex2.c
+++ tests/pseudocode_Ex2.c
@@ -5,14 +5,14 @@
 void Integral_Matrix(float *matrix, int M, int p, int q, int r) {
 
     /*
-      0 1 2 3 4 | M2
+      0 1 2 3 4 | j
     0 X V V V V
     1 X X V V V
     2 X X X V V
     3 X X X X V
     4 X X X X X
     -
-    M1
+    i
 
     The function creates a matrix with the respective definite integral
     solved. It would be stored in memory, so no value is returned.
@@ -22,7 +22,7 @@ void Integral_Matrix(float *matrix, int M, int p, int q, int r) {
 
     for (int i = 0; i<M-1; i++) {
         for (int j = i+1; j<M; j++) {
-           *(matrix+((i*M)+j)) = (float) Compute_Integral(i, j, p, q, r, 1000);
+           *(matrix+((i*M)+j)) = (float) Compute_Integral(i, j, p, q, r, 100);
             printf(" Value stored in matrix: %f\n", *(matrix+((i*M)+j)));
     }
   }
