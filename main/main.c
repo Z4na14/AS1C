fd8d8958bdf73a58ab7e19c692e866cc86cfd34f
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2024-10-15 17:07:50 +0200

    Pow library created

--- main/main.c
+++ main/main.c
@@ -1,8 +1,14 @@
 #include "../components/Compute_Integral/include/Compute_Integral.h"
 #include "../components/Integral_Matrix/include/Integral_Matrix.h"
+#include "../components/pow/include/pow.h"
 #include <stdio.h>
 
 
 void app_main(void) {
 	printf("Hello world\n");
+
+  float result = Compute_Integral(0, 10, 5, 3, 2, 1000);
+
+  printf("The result is: %f\n", result);
+
 }
