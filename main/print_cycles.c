1f2a2f0ec969f5bdc0592ff0daf2a6735420f545
Jorge Adrian Saghin Dudulea <zanajorgesaghin@gmail.com>
2024-10-31 16:00:42 +0100

    Los de arcos me van a comer la polla (han cambiado de nuevo el enunciado)

--- main/print_cycles.c
+++ main/print_cycles.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 
-void print_cycles(int cycles) {
+void print_variables(int cycles, int *matrix, int rows, int cols) {
   printf("Number of cycles used: %d\n", cycles);
-
+  
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      printf("%d ", *(matrix + (i * cols) + j));
+    }
+    printf("\n");
+  }
+  
   return;
 }
